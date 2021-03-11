e20f7be31a205849c7ede0eafba70badd17495b2
Hal9Thousand <halbrianthornton@gmail.com>
2021-03-11 15:59:57 +0000

    Lab4c

--- main.js
+++ main.js
@@ -27,7 +27,47 @@ const IPCIDR = require('ip-cidr');
  class IpAddress {
   constructor() { 
       
-    
+    /**
+    * Calculate and return the first host IP address from a CIDR subnet.
+    * @param {string} cidrStr - The IPv4 subnet expressed
+    *                 in CIDR format.
+    * @param {callback} callback - A callback function.
+    * @return {string} (firstIpAddress) - An IPv4 address.
+    */
+    function getFirstIpAddress(cidrStr, callback) {
+
+        // Initialize return arguments for callback
+        let firstIpAddress = null;
+        let _ipv6 = null;
+        let callbackError = null;
+
+        // Instantiate an object from the imported class and assign the instance to variable cidr.
+        const cidr = new IPCIDR(cidrStr);
+        // Initialize options for the toArray() method.
+        // We want an offset of one and a limit of one.
+        // This returns an array with a single element, the first host address from the subnet.
+        const options = {
+            from: 1,
+            limit: 1
+        };
+
+        // Use the object's isValid() method to verify the passed CIDR.
+        if (!cidr.isValid()) {
+            // If the passed CIDR is invalid, set an error message.
+            callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
+        } else {
+            // If the passed CIDR is valid, call the object's toArray() method.
+            // Notice the destructering assignment syntax to get the value of the first array's element.
+            [firstIpAddress] = cidr.toArray(options);  
+            _ipv6 = getIpv4MappedIpv6Address(firstIpAddress);
+        } 
+        
+        // Call the passed callback function.
+        // Node.js convention is to pass error data as the first argument to a callback.
+        // The IAP convention is to pass returned data as the first argument and error
+        // data as the second argument to the callback function.
+        return callback({ ipv4: firstIpAddress, ipv6 : _ipv6 }, callbackError);
+    }
 
     // IAP's global log object is used to output errors, warnings, and other
     // information to the console, IAP's log files, or a Syslog server.
